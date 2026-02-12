d9f24c759b44b224dc545254aae6ec77d823f374
md4i11 <vmforhomelab@outlook.com>
2026-02-12 12:13:14 +1000

    fixing tensorflow errors

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,16 +1,1 @@
-import os
-import numpy as np
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
-def load_data(image_dir, target_size=(224,224)):
-    images, labels = [], []
-    classes = os.listdir(image_dir)
-    for label in classes:
-        folder = os.path.join(image_dir, label)
-        for fname in os.listdir(folder):
-            path = os.path.join(folder, fname)
-            img = load_img(path, target_size=target_size, color_mode='rgb')
-            arr = img_to_array(img) / 255.0  # normalize
-            images.append(arr)
-            labels.append(label)
-    return np.array(images), np.array(labels)
